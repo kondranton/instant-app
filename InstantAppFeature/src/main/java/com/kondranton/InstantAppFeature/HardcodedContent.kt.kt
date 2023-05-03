adc90b866f7597e7a43fefdcd11da8016d6bf841
Anton Kondrashov <anton@offsetted.com>
2023-05-03 19:44:45 +0200

    Initial project

---
+++ InstantAppFeature/src/main/java/com/kondranton/InstantAppFeature/HardcodedContent.kt.kt
@@ -0,0 +1,11 @@
+package com.kondranton.InstantAppFeature
+
+class HardcodedContent {
+    companion object {
+        val urls = listOf(
+            "https://google.com",
+            "https://fb.com",
+            "https://apple.com"
+        )
+    }
+}
