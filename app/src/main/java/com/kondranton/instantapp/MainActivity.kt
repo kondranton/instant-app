b589abfe49e54d820a3fe06baab23fac027fd747
Anton Kondrashov <anton@offsetted.com>
2023-05-04 16:18:38 +0200

    Leave only one module

--- app/src/main/java/com/kondranton/instantapp/MainActivity.kt
+++ app/src/main/java/com/kondranton/instantapp/MainActivity.kt
@@ -1,43 +1,15 @@
 package com.kondranton.instantapp
 
+import HomeScreen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.kondranton.instantapp.ui.theme.InstantAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            InstantAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            HomeScreen()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    InstantAppTheme {
-        Greeting("Android")
     }
 }
